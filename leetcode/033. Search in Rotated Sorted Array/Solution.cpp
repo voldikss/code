b9ef1bc7f84e1f16bc8a6b764b855225798480fd
voldikss <dyzplus@gmail.com>
2019-08-25 10:31:53 +0800

    Upload

--- leetcode/033. Search in Rotated Sorted Array/Solution.cpp
+++ leetcode/033. Search in Rotated Sorted Array/Solution.cpp
@@ -14,33 +14,74 @@ using namespace std;
 class Solution {
 public:
   int search(vector<int> &nums, int target) {
-    int left, right, mid, rotate_pos;
-    left = 0;
-    right = nums.size();
-    mid = (left + right) / 2;
+    if (nums.size() == 0) {
+      return -1;
+    }
+    if (nums.size() == 1) {
+      if (nums[0] == target)
+        return 0;
+      else
+        return -1;
+    }
+
+    if (nums[0] < nums[nums.size() - 1]) {
+      return binarySearch(nums, 0, nums.size() - 1, target);
+    }
+
+    int begin, end, mid, rotate_pos;
+    begin = 0;
+    end = nums.size() - 1;
+    mid = (begin + end) / 2;
     // find the rotate position first
-    while (left <= right) {
-      if (nums[mid] >= nums[left] && nums[mid + 1] > nums[left]) {
-        right = mid;
-        mid = (left + right) / 2;
-      } else if (nums[mid] < nums[left] && nums[mid + 1] < nums[left]) {
-        left = mid;
-        mid = (left + right) / 2;
-      } else if (nums[mid] > nums[left] && nums[mid + 1] < nums[left]) {
-        rotate_pos = mid;
+    while (begin <= end) {
+      cout << begin << " " << mid << " " << end << endl;
+      if (nums[mid] >= nums[begin] && nums[mid + 1] > nums[begin]) {
+        begin = mid;
+        mid = (begin + end) / 2;
+      } else if (nums[mid] < nums[begin] && nums[mid + 1] < nums[begin]) {
+        end = mid;
+        mid = (begin + end) / 2;
+      } else if (nums[mid] >= nums[begin] && nums[mid + 1] < nums[begin]) {
+        rotate_pos = mid + 1;
         break;
       }
     }
-    printf("%d\n", rotate_pos);
-    // 二叉查找
-    // TODO
+    printf("rotate_pos is %d\n", rotate_pos);
 
-    return 0;
+    /* 二分查找 */
+    if (target > nums[0])
+      return binarySearch(nums, 0, rotate_pos, target);
+    else if (target < nums[0])
+      return binarySearch(nums, rotate_pos, nums.size() - 1, target);
+    else
+      return 0;
+  }
+
+  int binarySearch(const vector<int> &nums, int begin, int end, int target) {
+    int mid;
+    while (begin <= end) {
+      mid = (begin + end) / 2;
+      if (nums[mid] < target)
+        begin = mid + 1;
+      else if (nums[mid] > target)
+        end = mid - 1;
+      else
+        return mid;
+    }
+    return -1;
   }
 };
 
 int main() {
   Solution s;
-  vector<int> sample({4, 5, 6, 7, 0, 1, 2});
-  s.search(sample, 0);
+  // vector<int> sample({4, 5, 6, 7, 0, 1, 2});
+  // vector<int> sample({4, 5, 0, 1, 2});
+  // vector<int> sample({1, 2});
+  // vector<int> sample({1, 3});
+  // vector<int> sample({3, 1});
+  vector<int> sample({6, 7, 8, 1, 2, 3, 4, 5});
+  int ans = s.search(sample, 6);
+  cout << ans << endl;
+
+  // cout << s.binarySearch(sample, 0, 1, 1) << endl;
 }
