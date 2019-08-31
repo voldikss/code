7e4c52d26855a6a82dcf0f3ef8f56f4f7a901680
voldikss <dyzplus@gmail.com>
2019-08-31 23:39:26 +0800

    Upload

---
+++ leetcode/962. Maximum Width Ramp/Solution.cpp
@@ -0,0 +1,66 @@
+//============================================================================
+// FileName: Solution.cpp
+// Descrption:
+// Author: voldikss <dyzplus@gmail.com>
+// GitHub: https://github.com/voldikss
+//============================================================================
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <stack>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  int maxWidthRamp(vector<int> &A) {
+    vector<pair<int, int>> indexHeap;
+    int n = A.size();
+    for (int i = 0; i < n; ++i) {
+      indexHeap.push_back({i, A[i]});
+    }
+    sort(indexHeap.begin(), indexHeap.end(), cmp);
+
+    int maxWidth = 0;
+    int minIndex = indexHeap[0].first;
+    for (int i = 0; i < n; ++i) {
+      maxWidth = max(maxWidth, indexHeap[i].first - minIndex);
+      minIndex = min(minIndex, indexHeap[i].first);
+    }
+    return maxWidth;
+  }
+
+  static bool cmp(pair<int, int> a, pair<int, int> b) {
+    if (a.second == b.second)
+      return a.first < b.first;
+    else
+      return a.second < b.second;
+  }
+};
+
+/* 超时 */
+// class Solution {
+// public:
+//   int maxWidthRamp(vector<int> &A) {
+//     int i, k;
+//     int currWidth, maxWidth = 0;
+//     int n = A.size();
+//     for (i = 0; n - 1 - i > maxWidth; ++i)
+//       for (k = n - 1; k - i >= maxWidth; --k)
+//         if (A[k] >= A[i]) {
+//           currWidth = k - i;
+//           maxWidth = max(maxWidth, currWidth);
+//           break;
+//         }
+//     return maxWidth;
+//   }
+// };
+
+int main() {
+  Solution s;
+  vector<int> sample = {6, 0, 8, 2, 1, 5};
+  // vector<int> sample = {9, 8, 1, 0, 1, 9, 4, 0, 4, 1};
+  int res = s.maxWidthRamp(sample);
+  cout << res << endl;
+}
