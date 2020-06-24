807636a03c1a181027aded64badb9b4b5afb328f
voldikss <dyzplus@gmail.com>
2020-06-24 20:42:53 +0800

    Add Solution.py

---
+++ leetcode/039. Combination Sum/Solution.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def helper(nums1, nums2):
+            if sum(nums1) == target:
+                res.append(nums1)
+            elif sum(nums1) > target:
+                return
+
+            for i in range(len(nums2)):
+                nums = nums1.copy()
+                nums.append(nums2[i])
+                helper(nums, nums2[i:])
+
+        res = []
+        helper([], candidates)
+        return res
+
+
+s = Solution()
+print(s.combinationSum([2, 3, 6, 7], 7))
+print(s.combinationSum([], 7))
