1cfaa5896380f1d39f5458d2604801742fdf0656
voldikss <dyzplus@gmail.com>
2020-06-24 17:30:47 +0800

    Add Solution.cpp

---
+++ leetcode/036. Valid Sudoku/Solution.cpp
@@ -0,0 +1,65 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &vec) {
+    for (auto x : vec) os << x << " ";
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<vector<T>> &vec) {
+    for (auto &x : vec) os << x << endl;
+    return os;
+}
+
+class Solution {
+  public:
+    bool isValidSudoku(vector<vector<char>> &board) {
+        int row[9][9] = {0};
+        int col[9][9] = {0};
+        int box[9][9] = {0};
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == '.') continue;
+                int val = board[r][c] - '1';
+                if (row[r][val]) return false;
+                row[r][val] = 1;
+                if (col[c][val]) return false;
+                col[c][val] = 1;
+                int b = r / 3 * 3 + c / 3;
+                if (box[b][val]) return false;
+                box[b][val] = 1;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    vector<vector<char>> board = {
+        {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+        {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+        {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+        {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+        {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+        {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+        {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+        {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+    Solution s;
+    bool res = s.isValidSudoku(board);
+    cout << res << endl;
+    return 0;
+}
