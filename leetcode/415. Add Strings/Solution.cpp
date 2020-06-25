cbbcd6ec0beb46a383c6521ae4e2bfdbd648c853
voldikss <dyzplus@gmail.com>
2020-06-25 18:46:16 +0800

    Improve
    看了下评论区，发现只用 flag 不用 tmp 就能完成全程

--- leetcode/415. Add Strings/Solution.cpp
+++ leetcode/415. Add Strings/Solution.cpp
@@ -30,16 +30,13 @@ class Solution {
         int p1 = num1.size() - 1;
         int p2 = num2.size() - 1;
 
-        int flag = false;
-        int tmp = 0;
-        while (p1 >= 0 || p2 >= 0) {
-            tmp = flag;
-            if (p1 >= 0) tmp += num1[p1--] - '0';
-            if (p2 >= 0) tmp += num2[p2--] - '0';
-            flag = tmp / 10;
-            res.push_back(tmp % 10 + '0');
+        int flag = 0;
+        while (p1 >= 0 || p2 >= 0 || flag != 0) {
+            if (p1 >= 0) flag += num1[p1--] - '0';
+            if (p2 >= 0) flag += num2[p2--] - '0';
+            res.push_back(flag % 10 + '0');
+            flag /= 10;
         }
-        if (flag) res.push_back('1');
 
         reverse(res.begin(), res.end());
         return res;
