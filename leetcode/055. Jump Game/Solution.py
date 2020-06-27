1de97191b8076ef51f540cdcaa34169ae05d3879
voldikss <dyzplus@gmail.com>
2020-06-27 22:36:06 +0800

    Improve Solution.py

--- leetcode/055. Jump Game/Solution.py
+++ leetcode/055. Jump Game/Solution.py
@@ -3,29 +3,20 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        if len(nums) == 1:
-            return True
-        pos = len(nums) - 1
-        has_0 = False
-        while pos >= 0:
-            if nums[pos] == 0:
-                if not has_0:
-                    has_0 = True
-                    tmp = pos
-            elif has_0:
-                if nums[pos] > tmp - pos:
-                    has_0 = False
-                elif nums[pos] == tmp - pos and tmp == len(nums) - 1:
-                    has_0 = False
-            pos -= 1
-
-        return not has_0
+        max_pos = 0
+        for i in range(len(nums)):
+            if i > max_pos:
+                return False
+            max_pos = max(nums[i] + i, max_pos)
+            if max_pos >= len(nums) - 1:
+                return True
+        return False
 
 
 s = Solution()
 # print(s.canJump([0]))
-# print(s.canJump([3,2,1,0,4]))
-print(s.canJump([2, 0, 0]))
+# print(s.canJump([2, 0, 0]))
 # print(s.canJump([2, 3, 1, 1, 4]))
 # print(s.canJump([3, 2, 1, 0, 4]))
+print(s.canJump([0,2,3]))
 # print(s.canJump([2, 0]))
