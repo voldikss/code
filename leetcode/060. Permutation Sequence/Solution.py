613dea0e3cab4180d49a2d86d310af86fa4293fb
voldikss <dyzplus@gmail.com>
2020-07-02 17:35:20 +0800

    Improve
    Use pruning, inspired by https://leetcode-cn.com/problems/permutation-sequence/solution/hui-su-jian-zhi-python-dai-ma-java-dai-ma-by-liwei/

--- leetcode/060. Permutation Sequence/Solution.py
+++ leetcode/060. Permutation Sequence/Solution.py
@@ -3,22 +3,32 @@ from typing import List
 
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        count = 0
+        def helper(k: int, index: int):
+            if index == n:
+                return
 
-        def helper(path: str, residual: str):
-            nonlocal count
-            if len(residual) == 0:
-                count += 1
-                return path
+            for i in range(1, n + 1):
+                if used[i]:
+                    continue
+                cnt = factorial[n - index - 1]
+                if k > cnt:
+                    k -= cnt
+                    continue
 
-            for i in range(len(residual)):
-                res = helper(path + residual[i], residual[:i] + residual[i + 1 :])
-                if count == k:
-                    return res
+                used[i] = True
+                path.append(i)
+                helper(k, index + 1)
 
-        return helper("", "".join([str(x) for x in range(1, n + 1)]))
+        used = [False for _ in range(0, n + 1)]
+        factorial = [1 for _ in range(n + 1)]
+        for i in range(2, n + 1):
+            factorial[i] = i * factorial[i - 1]
+
+        path = []
+        helper(k, 0)
+        return "".join([str(i) for i in path])
 
 
 s = Solution()
-# print(s.getPermutation(3, 3))
-print(s.getPermutation(4, 9))
+print(s.getPermutation(3, 3))
+# print(s.getPermutation(4, 9))
