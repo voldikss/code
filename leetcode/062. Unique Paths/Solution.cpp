ac49d875d92f584250702f9d7d01152efe86377b
voldikss <dyzplus@gmail.com>
2020-07-03 16:16:24 +0800

    Add Solution.cpp

---
+++ leetcode/062. Unique Paths/Solution.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+template <typename T>
+ostream &operator<<(ostream &os, const list<T> &l) {
+    for (auto &i : l) os << i << " ";
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &vec) {
+    for (auto x : vec) os << x << " ";
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<vector<T>> &vec) {
+    for (auto &x : vec) os << x << endl;
+    return os;
+}
+
+class Solution {
+  public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 1));
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.uniquePaths(7, 3) << endl;
+    return 0;
+}
