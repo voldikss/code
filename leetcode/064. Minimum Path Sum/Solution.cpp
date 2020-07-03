5541fa989bb04025e6fe2674a3526a093fb4763d
voldikss <dyzplus@gmail.com>
2020-07-03 23:40:37 +0800

    Add Solution.cpp

---
+++ leetcode/064. Minimum Path Sum/Solution.cpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+template <typename T>
+ostream &operator<<(ostream &os, const list<T> &l) {
+    for (auto &i : l) os << i << " ";
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &vec) {
+    for (auto x : vec) os << x << " ";
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<vector<T>> &vec) {
+    for (auto &x : vec) os << x << endl;
+    return os;
+}
+
+class Solution {
+  public:
+    int minPathSum(vector<vector<int>> &grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        for (int i = 1; i < n; i++) {
+            grid[0][i] += grid[0][i - 1];
+        }
+
+        for (int i = 1; i < m; i++) {
+            grid[i][0] += grid[i - 1][0];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+};
+
+int main() {
+    Solution s;
+    // vector<vector<int>> grid = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+    vector<vector<int>> grid = {{1}};
+    auto res = s.minPathSum(grid);
+    cout << res << endl;
+    return 0;
+}
