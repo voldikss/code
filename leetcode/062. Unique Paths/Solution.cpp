bc206304cfa7cea7684686fedfb4040dc40dc8b1
voldikss <dyzplus@gmail.com>
2020-07-03 16:39:09 +0800

    Improve Solution.cpp

--- leetcode/062. Unique Paths/Solution.cpp
+++ leetcode/062. Unique Paths/Solution.cpp
@@ -33,13 +33,14 @@ ostream &operator<<(ostream &os, const vector<vector<T>> &vec) {
 class Solution {
   public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 1));
-        for (int i = 1; i < m; i++) {
+        vector<int> prev(n, 1);
+        vector<int> curr(n, 1);
+        for (int i = 1; i < m; i++)
             for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                curr[j] = curr[j - 1] + prev[j];
+                prev[j] = curr[j];
             }
-        }
-        return dp[m - 1][n - 1];
+        return curr[n - 1];
     }
 };
 
