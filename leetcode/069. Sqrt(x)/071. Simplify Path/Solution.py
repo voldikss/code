f2ba269abee576d0785c611b6623015bd453214f
voldikss <dyzplus@gmail.com>
2020-07-04 19:25:20 +0800

    Improve Solution.py

--- leetcode/069. Sqrt(x)/071. Simplify Path/Solution.py
+++ leetcode/069. Sqrt(x)/071. Simplify Path/Solution.py
@@ -1,28 +1,15 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
         l = path.split("/")
-        # print(l)
-        res = [""]
+        res = []
         for i in l:
-            if i != "":
+            if i == "..":
+                if res:
+                    res.pop()
+            elif i and i != ".":
                 res.append(i)
-            if i == ".":
-                res.pop()
-            elif i == "..":
-                if len(res) > 1:
-                    res.pop()
-                if len(res) > 1:
-                    res.pop()
 
-        while len(res) > 1 and res[-1] == "":
-            res.pop()
-        if len(res) == 0:
-            res.append("")
-        if len(res) == 1 and res[0] == "":
-            res.append("")
-        # res.append('')
-        print(res)
-        return "/".join(res)
+        return "/" + "/".join(res)
 
 
 s = Solution()
