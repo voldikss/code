378c31a647869e2f5203bf332da9e659b6344688
voldikss <dyzplus@gmail.com>
2020-07-04 11:23:26 +0800

    Improve Solution.cpp
    Inspired by https://leetcode-cn.com/problems/sqrtx/solution/x-de-ping-fang-gen-by-gpe3dbjds1/

--- leetcode/069. Sqrt(x)/Solution.cpp
+++ leetcode/069. Sqrt(x)/Solution.cpp
@@ -35,24 +35,18 @@ class Solution {
     int mySqrt(int x) {
         long left = 0;
         long right = x;
-        while (left < right) {
+        while (left <= right) {
             long mid = left + (right - left) / 2;
-            if (mid * mid > x) {
-                right = mid;
-            } else if (mid * mid < x) {
-                left = mid;
+            long res = mid * mid;
+            if (res > x) {
+                right = mid - 1;
+            } else if (res < x) {
+                left = mid + 1;
             } else {
                 return mid;
             }
-            if (right - 1 == left) {
-                if (right * right > x)
-                    return left;
-                else
-                    return right;
-            }
         }
-        // cout << left << " " << right << endl;
-        return 0;
+        return right;   // miao a! Here right must less equal than left since `while (left <= right)`
     }
 };
 
