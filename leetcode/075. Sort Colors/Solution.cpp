3ae0815a07c1adb138131686530ba074bb997a36
voldikss <dyzplus@gmail.com>
2020-07-05 16:56:50 +0800

    Improve Solution.cpp
    This ruinned my brain...

--- leetcode/075. Sort Colors/Solution.cpp
+++ leetcode/075. Sort Colors/Solution.cpp
@@ -33,34 +33,45 @@ ostream &operator<<(ostream &os, const vector<vector<T>> &vec) {
 class Solution {
   public:
     void sortColors(vector<int> &nums) {
-        int redCount = 0;
-        int whiteCount = 0;
-        int blueCount = 0;
+        int redIndex = 0;
+        int blueIndex = nums.size() - 1;
         for (int i = 0; i < nums.size(); i++) {
+            cout << nums << " " << i << endl;
+            if (i > blueIndex) break;
             if (nums[i] == 0)
-                redCount++;
-            else if (nums[i] == 1)
-                whiteCount++;
-            else
-                blueCount++;
+                nums[redIndex++] = 0;
+            else if (nums[i] == 2) {
+                // 1. handle end
+                while (1) {
+                    if (blueIndex < 0) return;
+                    if (nums[blueIndex] == 0) {
+                        if (blueIndex >= redIndex)
+                            nums[redIndex++] = 0;
+                        break;
+                    } else if (nums[blueIndex] == 1) {
+                        break;
+                    } else {
+                        blueIndex--;
+                    }
+                }
+                // 2. add this
+                if (i < blueIndex)
+                    nums[blueIndex--] = 2;
+            }
+            if (i >= redIndex && i <= blueIndex) nums[i] = 1;
         }
-        // cout << redCount << endl;
-        // cout << whiteCount << endl;
-        // cout << blueCount << endl;
-        int i = 0;
-        for (; i < redCount; i++)
-            nums[i] = 0;
-        for (; i < whiteCount + redCount; i++)
-            nums[i] = 1;
-        for (; i < nums.size(); i++)
-            nums[i] = 2;
     }
 };
 
 int main() {
     Solution s;
     // vector<int> nums = {2, 0, 2, 1, 1, 0};
-    vector<int> nums = {2, 0, 1};
+    // vector<int> nums = {2};
+    vector<int> nums = {0, 2};
+    // vector<int> nums = {1, 2};
+    // vector<int> nums = {2, 0, 1};
+    // vector<int> nums = {2, 0, 2};
+    // vector<int> nums = {2, 2};
     s.sortColors(nums);
     cout << nums << endl;
     return 0;
