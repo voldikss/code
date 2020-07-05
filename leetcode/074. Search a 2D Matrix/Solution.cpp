50df4c9a91ded9afe29b39219822dfa44ceaf95b
voldikss <dyzplus@gmail.com>
2020-07-05 14:26:34 +0800

    Improve Solution.cpp

--- leetcode/074. Search a 2D Matrix/Solution.cpp
+++ leetcode/074. Search a 2D Matrix/Solution.cpp
@@ -41,8 +41,16 @@ class Solution {
         for (int i = 0; i < m; i++) {
             if (matrix[i][0] > target) break;
             if (matrix[i][0] <= target && matrix[i][n - 1] >= target) {
-                for (int j = 0; j < n; j++) {
-                    if (matrix[i][j] == target) return true;
+                int left = 0;
+                int right = n - 1;
+                while (left <= right) {
+                    int mid = left + (right - left) / 2;
+                    if (matrix[i][mid] > target)
+                        right = mid - 1;
+                    else if (matrix[i][mid] < target)
+                        left = mid + 1;
+                    else
+                        return true;
                 }
                 break;
             }
@@ -53,9 +61,12 @@ class Solution {
 
 int main() {
     Solution s;
+    // vector<vector<int>> matrix = {{1, 3, 5, 7},
+    //                               {10, 11, 16, 20},
+    //                               {23, 30, 34, 50}};
     vector<vector<int>> matrix = {{1, 3, 5, 7},
                                   {10, 11, 16, 20},
                                   {23, 30, 34, 50}};
-    cout << s.searchMatrix(matrix, 3) << endl;
+    cout << s.searchMatrix(matrix, 13) << endl;
     return 0;
 }
