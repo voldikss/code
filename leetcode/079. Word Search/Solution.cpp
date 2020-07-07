20a9a1bb12d2e79118deeb08b55451d0286b0952
voldikss <dyzplus@gmail.com>
2020-07-07 09:42:01 +0800

    Improve Solution.cpp

--- leetcode/079. Word Search/Solution.cpp
+++ leetcode/079. Word Search/Solution.cpp
@@ -34,46 +34,31 @@ class Solution {
     vector<vector<int>> path;
 
   public:
-    void trace() {
-        for (auto &p : path) {
-            cout << "{" << p[0] << ", " << p[1] << "} ";
-        }
-    }
-
     bool exist(vector<vector<char>> &board, string word) {
-        if (board.size() == 1 && board[0].size() == 1) {
-            return word.size() == 1 && board[0][0] == word[0];
-        }
         for (int i = 0; i < board.size(); i++) {
             for (int j = 0; j < board[0].size(); j++) {
-                if (word[0] == board[i][j]) {
-                    if (word.size() == 1) return true;
-                    map<vector<int>, bool> used;
-                    used[{i, j}] = true;
-                    if (dfs(board, i, j, word, 0, used)) return true;
-                }
+                map<vector<int>, bool> used;
+                if (dfs(board, i, j, word, 0, used)) return true;
             }
         }
         return false;
     }
 
     bool dfs(vector<vector<char>> &board, int m, int n, string word, int x, map<vector<int>, bool> &used) {
+        if (board[m][n] != word[x]) return false;
+        if (x >= word.size() - 1) return true;
+        used[{m, n}] = true;
+
         vector<vector<int>> coors = {{m - 1, n}, {m, n - 1}, {m, n + 1}, {m + 1, n}};
         for (auto &co : coors) {
             int i = co[0];
             int j = co[1];
             if (i < 0 || i > board.size() - 1 || j < 0 || j > board[0].size() - 1) continue;
             if (used[{i, j}] == true) continue;
-            if (board[i][j] == word[x + 1]) {
-                used[{i, j}] = true;
-                if (x + 1 == word.size() - 1) return true;
-                if (dfs(board, i, j, word, x + 1, used)) {
-                    return true;
-                } else {
-                    used[{i, j}] = false;
-                }
-            }
+            if (dfs(board, i, j, word, x + 1, used)) return true;
         }
+
+        used[{m, n}] = false;
         return false;
     }
 };
@@ -87,13 +72,13 @@ int main() {
     // auto res = s.exist(board, "SEE");
     // auto res = s.exist(board, "ABCB");
 
-    // vector<vector<char>> board = {{'a'}};
-    // auto res = s.exist(board, "a");
+    vector<vector<char>> board = {{'a', 'a'}};
+    auto res = s.exist(board, "aa");
 
-    vector<vector<char>> board = {{'A', 'B', 'C', 'E'},
-                                  {'S', 'F', 'E', 'S'},
-                                  {'A', 'D', 'E', 'E'}};
-    auto res = s.exist(board, "ABCESEEEFS");
+    // vector<vector<char>> board = {{'A', 'B', 'C', 'E'},
+    //                               {'S', 'F', 'E', 'S'},
+    //                               {'A', 'D', 'E', 'E'}};
+    // auto res = s.exist(board, "ABCESEEEFS");
 
     cout << "-----------------------" << endl;
     cout << res << endl;
