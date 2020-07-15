a8e58efeebd51fbd16ae116c9d594a3be067b1af
voldikss <dyzplus@gmail.com>
2020-07-15 16:10:12 +0800

    Add Solution2.py
    Ref: https://leetcode-cn.com/problems/subsets-ii/solution/hui-su-suan-fa-by-powcai-6/

---
+++ leetcode/090. Subsets II/Solution2.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return []
+        nums.sort()
+        res = [[]]
+        cur = []
+        for i in range(len(nums)):
+            if nums[i] == nums[i - 1]:
+                cur = [tmp + [nums[i]] for tmp in cur]
+            else:
+                cur = [tmp + [nums[i]] for tmp in res]
+
+            res += cur
+
+        return res
+
+
+s = Solution()
+# res = s.subsetsWithDup([1, 2, 3])
+res = s.subsetsWithDup([1, 2, 2])
+print(res)
+print(len(res))
