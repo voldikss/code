c7519426010406f6b463efae96c0f3736ceac011
voldikss <dyzplus@gmail.com>
2020-07-15 10:50:29 +0800

    Improve Solution.py
    So cool!

--- leetcode/089. Gray Code/Solution.py
+++ leetcode/089. Gray Code/Solution.py
@@ -3,15 +3,11 @@ from typing import List
 
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        if n == 0:
-            return [0]
-        res = ["0" * n]
-        for i in range(n, 0, -1):
-            for num in res[::-1]:
-                res.append("".join((num[: i - 1], "1", num[i:])))
+        res = [0]
+        for i in range(n):
+            res += map(lambda x: x + 2 ** i, res[::-1])
 
-        res = map(lambda x: int(x, 2), res)
-        return list(res)
+        return res
 
 
 s = Solution()
