07fa98ac7e3933f2cb2fab49a3f8c66c223dbbd5
voldikss <dyzplus@gmail.com>
2020-07-16 16:25:24 +0800

    Add Solution4.cpp

---
+++ leetcode/206. Reverse Linked List/README.md
@@ -0,0 +1,9 @@
+- 递归法
+
+  - [Solution.cpp](./Solution.cpp)
+  - [Solution4.cpp](./Solution4.cpp)(更好)
+
+- 迭代法
+
+  - [Solution2.cpp](./Solution2.cpp)
+  - [Solution3.cpp](./Solution3.cpp)(更好)
---
+++ leetcode/206. Reverse Linked List/Solution4.cpp
@@ -0,0 +1,34 @@
+#include "../../templates/Solution.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    ListNode *reverseList(ListNode *head) {
+        if (!head || !head->next) return head;
+        ListNode *p = this->reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return p;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nodes = {1, 2, 3, 4, 5};
+    auto list = buildList(nodes);
+    auto res = s.reverseList(list);
+    cout << res << endl;
+    return 0;
+}
