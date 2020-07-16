e125a5b12af7856f6a2da645f18a555fbb5f2660
voldikss <dyzplus@gmail.com>
2020-07-16 13:59:48 +0800

    Update templates/Solution.cpp

--- leetcode/206. Reverse Linked List/Solution3.cpp
+++ leetcode/206. Reverse Linked List/Solution3.cpp
@@ -1,3 +1,4 @@
+#include "../../templates/Solution.cpp"
 #include <algorithm>
 #include <cassert>
 #include <climits>
@@ -11,40 +12,6 @@
 #include <unordered_map>
 #include <vector>
 using namespace std;
-
-template <typename T>
-ostream &operator<<(ostream &os, const list<T> &l) {
-    for (auto &i : l) os << i << " ";
-    return os;
-}
-
-template <typename T>
-ostream &operator<<(ostream &os, const vector<T> &vec) {
-    for (auto x : vec) os << x << " ";
-    return os;
-}
-
-template <typename T>
-ostream &operator<<(ostream &os, const vector<vector<T>> &vec) {
-    for (auto &x : vec) os << x << endl;
-    return os;
-}
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x)
-        : val(x), next(NULL) {
-    }
-};
-
-ostream &operator<<(ostream &os, const ListNode *head) {
-    while (head) {
-        os << head->val << " ";
-        head = head->next;
-    }
-    return os;
-}
 
 class Solution {
   public:
@@ -63,18 +30,10 @@ class Solution {
 };
 
 int main() {
-    Solution s;
-    ListNode *head = new ListNode(-1);
-    ListNode *p = head;
     vector<int> nodes = {1, 2, 3, 4, 5};
-    for (auto x : nodes) {
-        ListNode *node = new ListNode(x);
-        p->next = node;
-        p = p->next;
-    }
-    p->next = nullptr;
-
-    auto res = s.reverseList(head->next);
+    auto list = buildList(nodes);
+    Solution s;
+    auto res = s.reverseList(list);
     cout << res << endl;
     return 0;
 }
--- templates/Solution.cpp
+++ templates/Solution.cpp
@@ -12,12 +12,18 @@
 #include <vector>
 using namespace std;
 
+//============================================================================
+// List
+//============================================================================
 template <typename T>
 ostream &operator<<(ostream &os, const list<T> &l) {
     for (auto &i : l) os << i << " ";
     return os;
 }
 
+//============================================================================
+// Vector
+//============================================================================
 template <typename T>
 ostream &operator<<(ostream &os, const vector<T> &vec) {
     for (auto x : vec) os << x << " ";
@@ -30,10 +36,34 @@ ostream &operator<<(ostream &os, const vector<vector<T>> &vec) {
     return os;
 }
 
+//============================================================================
+// LinkedList
+//============================================================================
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x)
+        : val(x), next(NULL) {
+    }
+};
+
 ostream &operator<<(ostream &os, const ListNode *head) {
     while (head) {
         os << head->val << " ";
         head = head->next;
     }
     return os;
+}
+
+// build a linkedlist from vector of integers
+ListNode *buildList(const vector<int> &nodes) {
+    ListNode *head = new ListNode(-1);
+    ListNode *p = head;
+    for (auto x : nodes) {
+        ListNode *node = new ListNode(x);
+        p->next = node;
+        p = p->next;
+    }
+    p->next = nullptr;
+    return head->next;
 }
