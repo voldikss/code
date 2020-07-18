8599967241f9a59bb7afdf18737fe2139648d0c6
voldikss <dyzplus@gmail.com>
2020-07-18 22:27:29 +0800

    Add Solution.cpp

---
+++ leetcode/094. Binary Tree Inorder Traversal/Solution.cpp
@@ -0,0 +1,41 @@
+#include "../../vital/binary_tree.cpp"
+#include "../../vital/vector.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> inorderTraversal(TreeNode *root) {
+        vector<int> res;
+        if (!root) return res;
+        this->visit(root, res);
+        return res;
+    }
+
+    void visit(TreeNode *root, vector<int> &res) {
+        if (!root) return;
+        this->visit(root->left, res);
+        res.push_back(root->val);
+        this->visit(root->right, res);
+    }
+};
+
+int main() {
+    Solution s;
+    vector<variant<int, nullptr_t>> tree_nodes = {1, nullptr, 2, 3};
+    TreeNode *tree = build_binary_tree(tree_nodes);
+    // print_ascii_tree(tree);
+    cout << s.inorderTraversal(tree) << endl;
+}
