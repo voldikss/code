591caef6dcea9d5976b560c7b1223bddc4098f91
voldikss <dyzplus@gmail.com>
2020-07-19 09:52:01 +0800

    Add Solution5.cpp

---
+++ leetcode/0094. Binary Tree Inorder Traversal/Solution5.cpp
@@ -0,0 +1,50 @@
+#include "../../vital/binary_tree.cpp"
+#include "../../vital/vector.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+// Mirros algorithm
+class Solution {
+  public:
+    vector<int> inorderTraversal(TreeNode *root) {
+        vector<int> res;
+        if (!root) return res;
+        TreeNode *p = root;
+        while (root) {
+            if (root->left) {
+                p = root->left;
+                while (p->right) {
+                    p = p->right;
+                }
+                p->right = root;
+                TreeNode *tmp = root;
+                root = root->left;
+                tmp->left = nullptr;
+            } else {
+                res.emplace_back(root->val);
+                root = root->right;
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<variant<int, nullptr_t>> tree_nodes = {1, nullptr, 2, 3};
+    TreeNode *tree = build_binary_tree(tree_nodes);
+    // print_ascii_tree(tree);
+    cout << s.inorderTraversal(tree) << endl;
+}
