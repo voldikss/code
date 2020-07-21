afe3ac4a515deafa11b289b38de472b22b1ec63c
voldikss <dyzplus@gmail.com>
2020-07-21 08:59:18 +0800

    Add Solution.cpp

---
+++ leetcode/0096. Unique Binary Search Trees/Solution.cpp
@@ -0,0 +1,36 @@
+#include "../../vital/binary_tree.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    int numTrees(int n) {
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+};
+
+int main() {
+    Solution s;
+    // cout << s.numTrees(3) << endl;
+    cout << s.numTrees(4) << endl;
+    return 0;
+}
