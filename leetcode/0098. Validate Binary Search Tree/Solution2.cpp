4f259b40f91f4bb8f7eebadaf4b99c5dc6a116a4
voldikss <dyzplus@gmail.com>
2020-07-21 16:00:12 +0800

    Add Solution.cpp && Solution2.cpp

---
+++ leetcode/0098. Validate Binary Search Tree/Solution2.cpp
@@ -0,0 +1,40 @@
+#include "../../vital/binary_tree.cpp"
+#include "../../vital/vector.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+    long pre = INT64_MIN;
+
+  public:
+    bool isValidBST(TreeNode *root) {
+        if (!root) return true;
+        if (!this->isValidBST(root->left)) return false;
+        if (root->val <= pre) return false;
+        pre = root->val;
+        if (!this->isValidBST(root->right)) return false;
+        return true;
+    }
+};
+
+int main() {
+    Solution s;
+    // vector<variant<int, nullptr_t>> nodes = {};
+    // vector<variant<int, nullptr_t>> nodes = {2, 1, 3, nullptr, nullptr};
+    vector<variant<int, nullptr_t>> nodes = {5, 1, 4, nullptr, nullptr, 3, 6, nullptr, nullptr, nullptr, nullptr};
+    TreeNode *tree = build_binary_tree(nodes);
+    cout << s.isValidBST(tree) << endl;
+    return 0;
+}
