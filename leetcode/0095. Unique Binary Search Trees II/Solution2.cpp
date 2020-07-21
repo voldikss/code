c6ee5d794da0b214a62be6d1f168c54f322704d0
voldikss <dyzplus@gmail.com>
2020-07-21 14:32:39 +0800

    Add Solution2.cpp
    Ref: https://leetcode-cn.com/problems/unique-binary-search-trees-ii/solution/c-30xing-dai-ma-di-gui-by-black-hole/

---
+++ leetcode/0095. Unique Binary Search Trees II/Solution2.cpp
@@ -0,0 +1,58 @@
+#include "../../vital/binary_tree.cpp"
+#include "../../vital/vector.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    vector<TreeNode *> generateTrees(int n) {
+        if (n == 0) return {};
+        return this->helper(1, n);
+    }
+
+    vector<TreeNode *> helper(int start, int end) {
+        vector<TreeNode *> res;
+        if (start > end) {
+            res.emplace_back(nullptr);
+            return res;
+        }
+
+        if (start == end) {
+            res.emplace_back(new TreeNode(start));
+            return res;
+        }
+
+        for (int i = start; i <= end; i++) {
+            auto leftTrees = this->helper(start, i - 1);
+            auto rightTrees = this->helper(i + 1, end);
+            for (auto l : leftTrees) {
+                for (auto r : rightTrees) {
+                    TreeNode *node = new TreeNode(i);
+                    node->left = l;
+                    node->right = r;
+                    res.emplace_back(node);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution s;
+    auto res = s.generateTrees(3);
+    cout << res << endl;
+    return 0;
+}
