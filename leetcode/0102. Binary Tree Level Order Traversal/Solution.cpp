7ccbb84f8f1f825542c51cc88d5783e78d3f3814
voldikss <dyzplus@gmail.com>
2020-07-21 17:13:14 +0800

    Add Solution.cpp

---
+++ leetcode/0102. Binary Tree Level Order Traversal/Solution.cpp
@@ -0,0 +1,62 @@
+#include "../../vital/binary_tree.cpp"
+#include "../../vital/vector.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    vector<vector<int>> levelOrder(TreeNode *root) {
+        vector<vector<int>> res;
+        if (!root) return res;
+        queue<TreeNode *> q;
+        q.push(root);
+        int pre = 1;
+        int cur = 0;
+        vector<int> tmp;
+        while (!q.empty()) {
+            TreeNode *node = q.front();
+            tmp.emplace_back(node->val);
+
+            if (node->left) {
+                q.push(node->left);
+                cur++;
+            }
+
+            if (node->right) {
+                q.push(node->right);
+                cur++;
+            }
+
+            q.pop();
+            pre--;
+
+            if (pre == 0) {
+                res.emplace_back(tmp);
+                tmp.clear();
+                pre = cur;
+                cur = 0;
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<variant<int, nullptr_t>> nodes = {3, 9, 20, nullptr, nullptr, 15, 7};
+    TreeNode *tree = build_binary_tree(nodes);
+    cout << s.levelOrder(tree) << endl;
+    return 0;
+}
