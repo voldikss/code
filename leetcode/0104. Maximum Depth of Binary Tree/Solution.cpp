755b4894fca4aef8641d6b939a0da3080aa4d772
voldikss <dyzplus@gmail.com>
2020-07-21 19:54:38 +0800

    Add Solution.cpp

---
+++ leetcode/0104. Maximum Depth of Binary Tree/Solution.cpp
@@ -0,0 +1,32 @@
+#include "../../vital/binary_tree.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    int maxDepth(TreeNode *root) {
+        return this->maxDepth(root, 0);
+    }
+
+    int maxDepth(TreeNode *node, int depth) {
+        if (!node) return depth;
+        return max(this->maxDepth(node->left, depth + 1), this->maxDepth(node->right, depth + 1));
+    }
+};
+
+int main() {
+
+    return 0;
+}
