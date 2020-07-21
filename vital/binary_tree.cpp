5409cb7f43b616aaf03c19c7875e5ce11bce582e
voldikss <dyzplus@gmail.com>
2020-07-21 10:26:41 +0800

    Update vital/binary_tree.cpp

--- vital/binary_tree.cpp
+++ vital/binary_tree.cpp
@@ -20,10 +20,13 @@ struct TreeNode {
     TreeNode *left;
     TreeNode *right;
     TreeNode()
-        : left(nullptr), right(nullptr) {
+        : val(0), left(nullptr), right(nullptr) {
     }
     TreeNode(int x)
         : val(x), left(nullptr), right(nullptr) {
+    }
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {
     }
 };
 
