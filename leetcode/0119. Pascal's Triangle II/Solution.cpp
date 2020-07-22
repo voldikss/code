da3faf26faf3d55c862a4d23004c18e8e8ebd6e9
voldikss <dyzplus@gmail.com>
2020-07-22 16:21:58 +0800

    Add Solution.cpp

---
+++ leetcode/0119. Pascal's Triangle II/Solution.cpp
@@ -0,0 +1,36 @@
+#include "../../vital/vector.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> res;
+        res.emplace_back(1);
+        if (rowIndex == 0) return res;
+        while (rowIndex--) {
+            res.emplace_back(0);
+            vector<int> tmp(res);
+            for (int j = 1; j < res.size(); j++) res[j] += tmp[j - 1];
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.getRow(4) << endl;
+    return 0;
+}
