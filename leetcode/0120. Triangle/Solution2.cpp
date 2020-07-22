55feee8ac59bc3e431620d8a8158d1e35edb35dd
voldikss <dyzplus@gmail.com>
2020-07-22 23:15:47 +0800

    Add Solution2.cpp
    Inspired by https://leetcode-cn.com/problems/triangle/comments/2532

---
+++ leetcode/0120. Triangle/Solution2.cpp
@@ -0,0 +1,38 @@
+#include "../../vital/vector.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    int minimumTotal(vector<vector<int>> &triangle) {
+        for (int i = triangle.size() - 2; i >= 0; i--)
+            for (int j = 0; j < triangle[i].size(); j++)
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+        return triangle[0][0];
+    }
+};
+
+int main() {
+    Solution s;
+    // vector<vector<int>> triangle = {{-1},
+    //                                 {3, 2},
+    //                                 {-3, 1, -1}};
+    vector<vector<int>> triangle = {{2},
+                                    {3, 4},
+                                    {6, 5, 7},
+                                    {4, 1, 8, 3}};
+    cout << s.minimumTotal(triangle) << endl;
+    return 0;
+}
