057e136490a5551e551e827e40413f8540715923
voldikss <dyzplus@gmail.com>
2020-07-22 13:03:21 +0800

    Improve Solution.cpp
    之前因为 111 题 “子节点” 的坑留下了阴影，多写了些不必要的判断
    
    更新删除了一些不必要的判断，因为对于
    
        a
       /  \
      b   nullptr
    
    这种情况，由于 hasPathSum(a->right) 的结果一定是 false (因为有第一句`if (!root) return false`)，所以没有影响

--- leetcode/0112. Path Sum/Solution.cpp
+++ leetcode/0112. Path Sum/Solution.cpp
@@ -18,14 +18,7 @@ class Solution {
   public:
     bool hasPathSum(TreeNode *root, int sum) {
         if (!root) return false;
-        if (!root->left && !root->right) {
-            if (root->val == sum)
-                return true;
-            else
-                return false;
-        }
-        if (!root->left) return this->hasPathSum(root->right, sum - root->val);
-        if (!root->right) return this->hasPathSum(root->left, sum - root->val);
+        if (!root->left && !root->right) return root->val == sum;
         return this->hasPathSum(root->left, sum - root->val) || this->hasPathSum(root->right, sum - root->val);
     }
 };
