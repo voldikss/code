0845ac2eb6064896f30070d03fb1e1ace4381bf2
voldikss <dyzplus@gmail.com>
2020-07-22 15:20:34 +0800

    Add Solution.cpp
    启发自 94题的莫里斯算法~

---
+++ leetcode/0114. Flatten Binary Tree to Linked List/Solution.cpp
@@ -0,0 +1,39 @@
+#include "../../vital/binary_tree.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    void flatten(TreeNode *root) {
+        if (!root) return;
+        TreeNode *p = root->left;
+        while (p && p->right) p = p->right;
+        if (p) {
+            p->right = root->right;
+            root->right = root->left;
+            root->left = nullptr;
+        }
+        flatten(root->right);
+    }
+};
+
+int main() {
+    Solution s;
+    vector<variant<int, nullptr_t>> nodes = {1, nullptr, 2, 3, nullptr};
+    TreeNode *tree = build_binary_tree(nodes);
+    s.flatten(tree);
+    cout << tree << endl;
+    return 0;
+}
