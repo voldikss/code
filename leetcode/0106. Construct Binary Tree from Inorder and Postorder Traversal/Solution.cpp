23ee439a62c955e0d7e1195b4a68e92af75579fe
voldikss <dyzplus@gmail.com>
2020-07-22 09:02:24 +0800

    Add Solution.cpp
    其实翻转一下 postorder 数组就是上一题了

---
+++ leetcode/0106. Construct Binary Tree from Inorder and Postorder Traversal/Solution.cpp
@@ -0,0 +1,56 @@
+#include "../../vital/binary_tree.cpp"
+#include "../../vital/vector.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
+        reverse(postorder.begin(), postorder.end());
+        return this->helper(inorder, postorder);
+    }
+
+    TreeNode *helper(vector<int> &inorder, vector<int> &postorder) {
+        if (postorder.empty()) return nullptr;
+
+        TreeNode *node = new TreeNode(postorder[0]);
+        if (postorder.size() == 1) return node;
+
+        int i;
+        for (i = 0; i < inorder.size(); i++)
+            if (inorder[i] == postorder[0]) break;
+
+        vector<int> leftInOrder = vector<int>(inorder.begin(), inorder.begin() + i);
+        vector<int> rightInOrder = vector<int>(inorder.begin() + i + 1, inorder.end());
+
+        vector<int> leftPostOrder = vector<int>(postorder.end() - i, postorder.end());
+        vector<int> rightPostOrder = vector<int>(postorder.begin() + 1, postorder.end() - i);
+
+        node->left = this->helper(leftInOrder, leftPostOrder);
+        node->right = this->helper(rightInOrder, rightPostOrder);
+        return node;
+    }
+};
+
+int main() {
+    Solution s;
+    // vector<int> inorder = {9, 3, 15, 20, 7};
+    // vector<int> postorder = {9, 15, 7, 20, 3};
+    vector<int> inorder = {4, 2, 5, 1, 6, 3, 7};
+    vector<int> postorder = {4, 5, 2, 6, 7, 3, 1};
+    auto res = s.buildTree(inorder, postorder);
+    cout << res << endl;
+    return 0;
+}
