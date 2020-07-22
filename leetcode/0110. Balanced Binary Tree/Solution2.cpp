7d535e994a8f82f7303b4f346cd6eebcece2f94e
voldikss <dyzplus@gmail.com>
2020-07-22 11:33:58 +0800

    Add Solution2.cpp
    之前想着要自底向上，但是在 Solution.cpp 里面却写成了自顶向下
    看了题解才写完自底向上
    对我来说是挺好的题目，虽然是 Easy

---
+++ leetcode/0110. Balanced Binary Tree/Solution2.cpp
@@ -0,0 +1,35 @@
+#include "../../vital/binary_tree.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    bool isBalanced(TreeNode *root) {
+        return this->depth(root) > -1;
+    }
+
+    int depth(TreeNode *node) {
+        if (!node) return 0;
+        int left = this->depth(node->left);
+        int right = this->depth(node->right);
+        if (left == -1 || right == -1 || abs(left - right) > 1) return -1;
+        return max(left, right) + 1;
+    }
+};
+
+int main() {
+
+    return 0;
+}
