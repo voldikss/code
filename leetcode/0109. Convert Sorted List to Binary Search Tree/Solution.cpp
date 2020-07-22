fecd401e1e653b0f27aeac9c49b74c84519d5f58
voldikss <dyzplus@gmail.com>
2020-07-22 10:45:37 +0800

    Add Solution.cpp

---
+++ leetcode/0109. Convert Sorted List to Binary Search Tree/Solution.cpp
@@ -0,0 +1,50 @@
+#include "../../vital/binary_tree.cpp"
+#include "../../vital/linkedlist.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    TreeNode *sortedListToBST(ListNode *head) {
+        return this->helper(head, nullptr);
+    }
+
+  private:
+    TreeNode *helper(ListNode *begin, ListNode *end) {
+        if (begin == end) return nullptr;
+        ListNode *faster = begin;
+        ListNode *slower = begin;
+        while (true) {
+            faster = faster->next;
+            if (faster == end) break;
+            faster = faster->next;
+            if (faster == end) break;
+            slower = slower->next;
+        }
+        TreeNode *node = new TreeNode(slower->val);
+        node->left = this->helper(begin, slower);
+        node->right = this->helper(slower->next, end);
+        return node;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nodes = {-10, -3, 0, 5, 9};
+    ListNode *list = build_linkedlist(nodes);
+    auto res = s.sortedListToBST(list);
+    cout << res << endl;
+    return 0;
+}
