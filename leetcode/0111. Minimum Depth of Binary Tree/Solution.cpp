1be39c2c68517f74ad0764fb8ff81e8e58d75f64
voldikss <dyzplus@gmail.com>
2020-07-22 12:16:06 +0800

    Add Solution.cpp
    差点被坑

---
+++ leetcode/0111. Minimum Depth of Binary Tree/Solution.cpp
@@ -0,0 +1,41 @@
+#include "../../vital/binary_tree.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+    long res = INT64_MAX;
+
+  public:
+    int minDepth(TreeNode *root) {
+        return this->helper(root, 0);
+    }
+
+  private:
+    int helper(TreeNode *root, int depth) {
+        if (!root) return depth;
+        if (depth > this->res) return depth;
+        if (!root->left && !root->right) return depth + 1;
+        if (!root->left) return this->helper(root->right, depth + 1);
+        if (!root->right) return this->helper(root->left, depth + 1);
+        int curdepth = min(this->helper(root->left, depth + 1), this->helper(root->right, depth + 1));
+        this->res = curdepth;
+        return curdepth;
+    }
+};
+
+int main() {
+
+    return 0;
+}
