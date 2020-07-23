8318e1a1aa603ab051d9362907333fe5b563fb64
voldikss <dyzplus@gmail.com>
2020-07-23 22:52:44 +0800

    Add Solution.cpp
    没想出来。。。

---
+++ leetcode/0122. Best Time to Buy and Sell Stock II/Solution.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    int maxProfit(vector<int> &prices) {
+        int res = 0;
+        for (int i = 0; i < prices.size() - 1; i++) res += max(prices[i + 1] - prices[i], 0);
+        return res;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> prices = {};
+    // vector<int> prices = {7, 1, 5, 3, 6, 4};
+    cout << s.maxProfit(prices) << endl;
+    return 0;
+}
