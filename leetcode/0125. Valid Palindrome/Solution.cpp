9b237e93ecb5c4a6216c002abb146afc45891e4b
voldikss <dyzplus@gmail.com>
2020-08-03 18:04:00 +0800

    Add Solution.cpp

---
+++ leetcode/0125. Valid Palindrome/Solution.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    bool isPalindrome(string s) {
+        int p = 0;
+        int q = s.size() - 1;
+        while (p < q) {
+            while (!isalnum(s[p]) && p <= q) p++;
+            while (!isalnum(s[q]) && p <= q) q--;
+
+            if (p >= q) return true;
+            if (isdigit(s[p])) {
+                if (s[p++] != s[q++]) return false;
+            } else {
+                if (tolower(s[p++]) != tolower(s[q++])) return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution s;
+    // cout << s.isPalindrome("A man, a plan, a canal: Panama") << endl;
+    // cout << s.isPalindrome("race a car") << endl;
+    // cout << s.isPalindrome("0P") << endl;
+    // cout << s.isPalindrome("pP") << endl;
+    cout << s.isPalindrome(".,") << endl;
+    return 0;
+}
