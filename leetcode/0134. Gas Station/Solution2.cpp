8e110980efc5c1e6b8e478dbadff304a57e6b016
voldikss <dyzplus@gmail.com>
2020-08-14 22:35:46 +0800

    Add Solution2.cpp
    带电粒子在交变电场中的运动...

---
+++ leetcode/0134. Gas Station/Solution2.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+        int length = gas.size();
+        int spare = 0;
+        int minSpare = INT_MAX;
+        int minSpareId = 0;
+
+        for (int i = 0; i < length; i++) {
+            spare += gas[i] - cost[i];
+            if (spare < minSpare) {
+                minSpare = spare;
+                minSpareId = i;
+            }
+        }
+
+        if (spare < 0) {
+            return -1;
+        } else {
+            return (minSpareId + 1) % length;
+        }
+    }
+};
+
+int main() {
+    vector<int> gas = {1, 2, 3, 4, 5};
+    vector<int> cost = {3, 4, 5, 1, 2};
+    // vector<int> gas = {2, 3, 4};
+    // vector<int> cost = {3, 4, 3};
+    Solution s;
+    auto res = s.canCompleteCircuit(gas, cost);
+    cout << res << endl;
+    return 0;
+}
