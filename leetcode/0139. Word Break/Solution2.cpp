8e124157443ab24c79131e82dbb55cb2429e9bd0
voldikss <dyzplus@gmail.com>
2020-08-26 17:18:27 +0800

    Add Solution2.cpp

---
+++ leetcode/0139. Word Break/Solution2.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    bool wordBreak(string s, vector<string> &wordDict) {
+        set<string> wordSet;
+        for (auto &w : wordDict) {
+            wordSet.insert(w);
+        }
+
+        vector<bool> dp(s.length());
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (dp[j] && wordSet.find(s.substr(j, i - j)) != wordSet.end()) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+};
+
+int main() {
+    Solution s;
+    // string str = "leetcode";
+    // vector<string> wordDict = {"leet", "code"};
+    string str = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
+    vector<string> wordDict = {"a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"};
+    cout << s.wordBreak(str, wordDict) << endl;
+    return 0;
+}
