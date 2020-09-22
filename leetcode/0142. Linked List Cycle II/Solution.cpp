8068a0fb121a960763b9d3952b12c7001b73220e
voldikss <dyzplus@gmail.com>
2020-09-22 09:15:14 +0800

    Add Solution.cpp

---
+++ leetcode/0142. Linked List Cycle II/Solution.cpp
@@ -0,0 +1,47 @@
+#include "../../vital/linkedlist.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    ListNode *detectCycle(ListNode *head) {
+        if (!head || !head->next) return nullptr;
+        ListNode *meet = nullptr;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (true) {
+            slow = slow->next;
+            fast = fast->next;
+            if (!fast || !fast->next) return nullptr;
+            fast = fast->next;
+            if (slow == fast) {
+                meet = slow;
+                break;
+            }
+        }
+        ListNode *p1 = head;
+        ListNode *p2 = meet;
+        while (p1 != p2) {
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        return p1;
+    }
+};
+
+int main() {
+
+    return 0;
+}
