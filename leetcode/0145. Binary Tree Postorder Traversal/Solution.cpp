5d953a7e04130aaa094062a3a10696448f1642a8
voldikss <dyzplus@gmail.com>
2020-09-23 23:09:25 +0800

    Add Solution.cpp

---
+++ leetcode/0145. Binary Tree Postorder Traversal/Solution.cpp
@@ -0,0 +1,44 @@
+#include "../../vital/binary_tree.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> postorderTraversal(TreeNode *root) {
+        vector<int> res;
+        if (!root) return res;
+        stack<TreeNode *> st;
+        map<TreeNode *, bool> visited;
+        TreeNode *p = nullptr;
+        st.push(root);
+        while (!st.empty()) {
+            p = st.top();
+            if (!visited[p]) {
+                if (p->right) st.push(p->right);
+                if (p->left) st.push(p->left);
+                visited[p] = true;
+            } else {
+                st.pop();
+                res.push_back(p->val);
+            }
+        }
+        return res;
+    }
+};
+
+int main() {
+
+    return 0;
+}
