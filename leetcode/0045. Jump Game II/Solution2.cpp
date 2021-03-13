1736bc276f29bd8054df0cb7ed2d20b15d0a3ad2
alantamfans <alan@tam.fans>
2021-03-13 22:47:28 +0800

    update

---
+++ leetcode/0045. Jump Game II/Solution2.cpp
@@ -0,0 +1,48 @@
+#include "../../include/vector.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <cstddef>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    int jump(vector<int>& nums) {
+        // TODO
+        int end = 0;
+        int step = 0;
+        int maxpos = 0;
+        int i = 0;
+        while (end < nums.size() - 1) {
+            maxpos = max(maxpos, nums[i] + i);
+            if (i == end) {
+                step++;
+                end = maxpos;
+            }
+            i++;
+        }
+        return step;
+    }
+};
+
+int main(int argc, char** argv) {
+    Solution s;
+    // auto nums = vector<int>{2, 3, 1, 1, 4};
+    auto nums = vector<int>{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 0};
+    // auto nums = vector<int>{2, 3, 1, 1, 4};
+    // auto nums = vector<int>{5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0};
+    // auto nums = vector<int>{2, 3, 0, 1, 4};
+    auto res = s.jump(nums);
+    cout << "res: " << res << endl;
+    return 0;
+}
---
+++ leetcode/0455. Assign Cookies/Solution.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <bits/c++config.h>
+#include <cassert>
+#include <climits>
+#include <cstddef>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <variant>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        size_t gindex = 0;
+        size_t sindex = 0;
+
+        while (gindex < g.size() && sindex < s.size()) {
+            if (g[gindex] <= s[sindex]) {
+                gindex++;
+            }
+            sindex++;
+        }
+        return gindex;
+    }
+};
+
+int main(int argc, char** argv) {
+    Solution sol;
+    auto g = vector<int>{7, 8, 9, 10}, s = vector<int>{5, 6, 7, 8};
+    auto res = sol.findContentChildren(g, s);
+    cout << res << endl;
+    return 0;
+}
