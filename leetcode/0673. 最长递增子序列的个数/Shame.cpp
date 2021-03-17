01b6c1e5f1fa62832c4255dff2124b88aae27312
alantamfans <alan@tam.fans>
2021-03-17 22:48:52 +0800

    update

---
+++ leetcode/0673. 最长递增子序列的个数/Shame.cpp
@@ -0,0 +1,88 @@
+#include "../../include/vector.cpp"
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+// 佟湘玉：把我都给气笑咧。。。
+class Solution {
+  public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int len = nums.size();
+        vector<int> dp(len, 1);
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        cout << dp << endl;
+
+        int maxlength = *max_element(dp.begin(), dp.end());
+        int i = len - 1;
+        while (i >= 0) {
+            if (dp[i] == maxlength) break;
+            i--;
+        }
+        int idx_of_2 = 0;
+        while (idx_of_2 < len) {
+            if (dp[idx_of_2] == 2) break;
+            idx_of_2++;
+        }
+        vector<int> count(len + 1, 0);
+        while (i >= 0) {
+            cout << "ddd" << dp[i] << endl;
+            if (dp[i] == 1) {
+                if (i > idx_of_2) {
+                    i--;
+                    continue;
+                }
+                if (maxlength >= 2 && nums[i] >= nums[idx_of_2]) {
+                    cout << nums[i] << endl;
+                    break;
+                }
+                count[dp[i]]++;
+            } else if (dp[i] == maxlength - 1) {
+                count[dp[i]]++;
+                maxlength--;
+            } else if (dp[i] == maxlength) {
+                count[dp[i]]++;
+            }
+            i--;
+        }
+
+        cout << count << endl;
+
+        int res = 1;
+        for (const auto& x : count) {
+            if (x != 0) res *= x;
+        }
+
+        return res;
+    }
+};
+
+int main(int argc, char** argv) {
+    Solution s;
+    // vector<int> nums{1, 3, 5, 4, 7};   // 2
+    // vector<int> nums{1};
+    // vector<int> nums{2, 2, 2, 2, 2}; // 5
+    // vector<int> nums{5, 4, 9, 8};   // 4
+    // vector<int> nums{9, 3, 4, 1, 0, 5};   // 1
+    // vector<int> nums{8, 4, 0, 4, 3, 9, 10, 3, 4};   // 2
+    vector<int> nums{1,2,4,3,5,4,7,2};   // 
+    // vector<int> nums{4, 10, 4};   // 1
+    auto res = s.findNumberOfLIS(nums);
+    cout << res << endl;
+    return 0;
+}
---
+++ leetcode/0673. 最长递增子序列的个数/Solution.cpp
@@ -0,0 +1,60 @@
+#include "../../include/vector.cpp"
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int len = nums.size();
+        vector<int> dp(len, 1);
+        vector<int> count(len, 1);
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = max(dp[i], dp[j] + 1);
+                        count[i] = count[j];
+                    } else if (dp[j] + 1 == dp[i]) {
+                        count[i] += count[j];
+                    }
+                }
+            }
+        }
+
+        int max_len = *max_element(dp.begin(), dp.end());
+        int res = 0;
+        for (int i = 0; i < len; i++) {
+            if (dp[i] == max_len) {
+                res += count[i];
+            }
+        }
+
+        return res;
+    }
+};
+
+int main(int argc, char** argv) {
+    Solution s;
+    // vector<int> nums{1, 3, 5, 4, 7};   // 2
+    // vector<int> nums{1};
+    // vector<int> nums{2, 2, 2, 2, 2}; // 5
+    // vector<int> nums{5, 4, 9, 8};   // 4
+    // vector<int> nums{9, 3, 4, 1, 0, 5};   // 1
+    // vector<int> nums{8, 4, 0, 4, 3, 9, 10, 3, 4};   // 2
+    // vector<int> nums{1, 2, 4, 3, 5, 4, 7, 2};   //
+    vector<int> nums{1, 2, 3, 1, 2, 3, 1, 2, 3};   //
+    // vector<int> nums{4, 10, 4};   // 1
+    auto res = s.findNumberOfLIS(nums);
+    cout << res << endl;
+    return 0;
+}
