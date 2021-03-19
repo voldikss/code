0eb82ff9beca2fd8978f71143aea46064ba0977b
alantamfans <alan@tam.fans>
2021-03-19 14:59:42 +0800

    add
    * 被面到了这道题，公司好照顾面试者啊，挑了前100以内的一道...可惜因为我给
      参数加了个 const 导致 sort 报错，找了半天。。。sigh and cry...

---
+++ leetcode/0056. 合并区间/Solution.cpp
@@ -0,0 +1,48 @@
+#include "../../include/vector.cpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        std::sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0];
+        });
+        int len = intervals.size();
+        int p2 = 1;
+        vector<int> prev = intervals[0];
+        while (p2 < len) {
+            auto curr = intervals[p2];
+            //     cout << curr[0] << ' ' << curr[1] << endl;
+            if (prev[1] >= curr[0]) {
+                if (prev[1] <= curr[1]) prev[1] = curr[1];
+            } else {
+                res.push_back(prev);
+                prev = curr;
+            }
+            p2++;
+        }
+        res.push_back(prev);
+        return res;
+    }
+};
+
+int main(int argc, char** argv) {
+    Solution s;
+    vector<vector<int>> intervals{{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+    auto res = s.merge(intervals);
+    cout << res << endl;
+    return 0;
+}
