ce01dc3d72cd6be89152f90e8c10885f74072c20
alantamfans <alan@tam.fans>
2021-04-04 09:34:53 +0800

    update

--- leetcode/0260. 只出现一次的数字 III/Solution2.cpp
+++ leetcode/0260. 只出现一次的数字 III/Solution2.cpp
@@ -12,24 +12,17 @@
 #include <vector>
 using namespace std;
 
+// 该解法提供了一种方便的寻找二进制1所在最低位的方法
 class Solution {
   public:
     vector<int> singleNumber(vector<int>& nums) {
-        int xorall = 0;
+        unsigned xorall = 0;
         for (const auto& x : nums) xorall ^= x;
-        int count = 0;
-        // 不断右移来获取1所在的最高位，原来 C++ 是算术右移！
-        while (xorall != 1) {
-            xorall = (int)((unsigned int)xorall >> 1);
-            cout << "count: " << count;
-            count++;
-        }
-        // 再左移回去，即可作为 mask
-        xorall <<= count;
+        int mask = xorall & (-xorall);
         int n1 = 0;
         int n2 = 0;
         for (const auto& x : nums) {
-            if ((x & xorall) == 0) {
+            if ((x & mask) == 0) {
                 n1 ^= x;
             } else {
                 n2 ^= x;
