09704e09e589b552120a2b2aae615d8ca00f3014
alantamfans <alan@tam.fans>
2021-04-05 11:52:33 +0800

    update

---
+++ leetcode/0088. 合并两个有序数组/Solution.cpp
@@ -0,0 +1,51 @@
+#include "../../include/global.hpp"
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p3 = nums1.size() - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[p3] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p3] = nums2[p2];
+                p2--;
+            }
+            p3--;
+        }
+        if (p2 >= 0) {
+            while (p2 >= 0) nums1[p3--] = nums2[p2--];
+        }
+    }
+};
+
+int main() {
+    Solution s;
+    // vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+    // int m = 3;
+    // vector<int> nums2 = {2, 5, 6};
+    // int n = 3;
+    vector<int> nums1 = {0};
+    int m = 0;
+    vector<int> nums2 = {1};
+    int n = 1;
+    s.merge(nums1, m, nums2, n);
+    cout << nums1 << endl;
+    return 0;
+}
