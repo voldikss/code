ed3cb2bf8e5cfb19b15c28260c53b1c620647efa
alantamfans <alan@tam.fans>
2021-04-11 15:53:40 +0800

    update

---
+++ leetcode/0232. 用栈实现队列/Solution.cpp
@@ -0,0 +1,68 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class MyQueue {
+  public:
+    /** Initialize your data structure here. */
+    MyQueue() {
+        while (!istack.empty()) istack.pop();
+        while (!ostack.empty()) ostack.pop();
+    }
+
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        istack.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        if (ostack.empty()) {
+            while (!istack.empty()) {
+                ostack.push(istack.top());
+                istack.pop();
+            }
+        }
+        int top = ostack.top();
+        ostack.pop();
+        return top;
+    }
+
+    /** Get the front element. */
+    int peek() {
+        if (ostack.empty()) {
+            while (!istack.empty()) {
+                ostack.push(istack.top());
+                istack.pop();
+            }
+        }
+        return ostack.top();
+    }
+
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return istack.empty() && ostack.empty();
+    }
+
+  private:
+    stack<int> istack;
+    stack<int> ostack;
+};
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
