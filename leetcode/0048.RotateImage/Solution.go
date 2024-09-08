adb0ee95190f8e0083c2cd80a27aa833eecb2f0d
voldikss <dyzplus@gmail.com>
2024-09-08 18:24:00 +0800

    update

---
+++ leetcode/0048.RotateImage/Solution.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n; i++ {
+		for j := 0; j < i; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < n/2; j++ {
+			matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+		}
+	}
+}
+
+func main() {
+	matrix := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+	rotate(matrix)
+	fmt.Println(matrix)
+}
